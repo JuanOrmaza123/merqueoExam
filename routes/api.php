a02fbb951a2ec06c6d459273b24f6429a4e52502
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-19 10:03:42 -0500

    api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('signup', 'Api\AuthController@signUp');
+
+    Route::group(['middleware' => ['api']], function () {
+        Route::get('logout', 'Api\AuthController@logout');
+        Route::get('user', 'Api\AuthController@user');
+    });
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
